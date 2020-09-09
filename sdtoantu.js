52b33f383575c57239b6d49b83aeb867a60bef99
unknown <dongcoxanh1001@gmail.com>
2020-09-09 11:52:52 +0700

    add file

---
+++ sdtoantu.js
@@ -0,0 +1,11 @@
+let inputwidth
+let inputheight
+
+inputheight = prompt("enter the height")
+inputwidth = prompt("enter the width")
+
+let width = parseInt(inputwidth)
+let height = parseInt(inputheight)
+
+let area = width * height
+document.write("The area is: " + area)
